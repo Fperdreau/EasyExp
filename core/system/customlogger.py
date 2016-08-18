36500c885a3db8b17a4301b7474a1ae7fb3f8bed
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-08-18 13:41:01 +0200

    fixed handling of logger

--- core/system/customlogger.py
+++ core/system/customlogger.py
@@ -1,6 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Florian Perdreau (f.perdreau@donders.ru.nl)
+#
+# This file is part of EasyExp
+#
+# Copyright (C) 2016 Florian Perdreau, Radboud University Nijmegen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 
@@ -12,13 +28,15 @@ class CustomLogger(object):
 
     Usage:
     >>> # Instantiate CustomLogger
-    >>> mylogger = CustomLogger('root', 'log_file.log', 'info')
+    >>> mylogger = CustomLogger(__name__, 'log_file.log', 'info')
     >>> # Output a message
     >>> mylogger.logger.info('Hello, world!')
-
+    >>> mylogger.logger.debug('Hello, world!')
+    >>> mylogger.logger.warning('Hello, world!')
     """
 
     default_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    allowed_level = ['debug', 'info', 'warning', 'critical', 'fatal']
 
     def __init__(self, name='', file_name='log.txt', level='debug', format_str=None):
         """
@@ -33,13 +51,20 @@ class CustomLogger(object):
         """
 
         if format_str is None:
-            self._formatter = CustomLogger.default_format
+            format_str = CustomLogger.default_format
 
         self._name = name
         self._file_name = file_name
 
+        # Set logger
+        self.logger = logging.getLogger(self._name)
+
         # Set output format
-        self._handler.setFormatter(logging.Formatter(format_str))
+        formatter = logging.Formatter(format_str)
+
+        # Set handler and output format
+        self._handler = logging.FileHandler(self._file_name)
+        self._handler.setFormatter(formatter)
 
         # Set verbose level
         self._set_level(level)
@@ -48,14 +73,6 @@ class CustomLogger(object):
 
         # Add handler to logger
         self.logger.addHandler(self._handler)
-
-    @property
-    def _handler(self):
-        return logging.FileHandler(self._file_name)
-
-    @property
-    def logger(self):
-        return logging.getLogger(self._name)
 
     def _set_level(self, level):
         """
@@ -68,9 +85,16 @@ class CustomLogger(object):
         -------
 
         """
+        if level not in CustomLogger.allowed_level:
+            raise AttributeError('"{}" is not a valid verbose level. '
+                                 'Possible options are debug, info, warning, critical or fatal')
         if level == 'debug':
             self._level = logging.DEBUG
         elif level == 'info':
             self._level = logging.INFO
         elif level == 'warning':
             self._level = logging.WARNING
+        elif level == 'critical':
+            self._level = logging.CRITICAL
+        elif level == 'fatal':
+            self._level = logging.FATAL
