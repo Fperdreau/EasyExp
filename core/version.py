860a3014192f923fc8cb5a5189d2171f242e878c
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-08-18 17:10:17 +0200

    Application information

---
+++ core/version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Florian Perdreau"
+__copyright__ = "Copyright 2015, Florian Perdreau"
+__license__ = "GPL"
+__version__ = "1.0.0 - Alpha"
+__maintainer__ = "Florian Perdreau"
+__email__ = "f.perdreau@donders.ru.nl"
+__status__ = "Production"
