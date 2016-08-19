0e445a1ff02a60297d0a8d212bed0a8a0142fe93
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-08-19 12:11:57 +0200

    added file header

--- core/version.py
+++ core/version.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# This file is part of EasyExp
+#
+# Copyright (C) 2016 Florian Perdreau, Radboud University Nijmegen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 __author__ = "Florian Perdreau"
-__copyright__ = "Copyright 2015, Florian Perdreau"
+__copyright__ = "Copyright 2016, Florian Perdreau"
 __license__ = "GPL"
 __version__ = "1.0.0 - Alpha"
 __maintainer__ = "Florian Perdreau"
