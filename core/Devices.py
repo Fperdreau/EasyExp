5416f52e13135bdb8935a3c37c918f0a37dc7bae
Florian Perdreau <florian.perdreau@gmail.com>
2016-08-25 13:18:40 +0200

    added handler for devices.json file

--- core/Devices.py
+++ core/Devices.py
@@ -26,8 +26,12 @@ from __future__ import print_function
 import sys
 import types
 
+import time
+
 from core.Config import ConfigFiles
 import logging
+
+from core.Core import Core
 
 
 class Devices(object):
@@ -35,15 +39,16 @@ class Devices(object):
     Dependency container for devices
     """
 
-    def __init__(self, file_name):
+    def __init__(self, core=Core):
         """
         Devices constructor
-        Parameters
-        ----------
-        file_name
+        :param core: EasyExp.core instance
+        :type core: Core
         """
+        self.__core = core
         self.devices = dict()
-        self.settingsFile = ConfigFiles(file_name)
+        self.devices_file = ConfigFiles('{}/devices.json'.format(core.folders['expFolder']))
+        self.settingsFile = core.config
         self.__logger = logging.getLogger('EasyExp')
 
     def init(self):
@@ -53,14 +58,21 @@ class Devices(object):
         -------
 
         """
-        self.settingsFile.load()
-        for label, params in self.settingsFile.data.iteritems():
-            device = Devices.str_to_class(label)
-            if label not in self.devices:
+        self.devices_file.load()
+        for device_name, params in self.devices_file.data.iteritems():
+            device = Devices.str_to_class(device_name)
+            if device_name not in self.devices:
                 try:
-                    self.devices[label] = device(**params)
+                    if hasattr(device, "user_file"):
+                        user_file = '{}/{}_{}.txt'.format(self.__core.user.dftName, device_name,
+                                                          time.strftime('%d-%m-%Y_%H%M%S'))
+                        params.update({"user_file": user_file})
+
+                    self.devices[device_name] = device(**params)
                 except AttributeError:
-                    raise AttributeError('Could not instantiate {}'.format(label))
+                    raise AttributeError('Could not instantiate {}'.format(device_name))
+
+
 
     @staticmethod
     def str_to_class(field):
