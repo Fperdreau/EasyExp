212252c671ec9fc0fb81817f92176f129f39367c
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-10-06 10:48:32 +0200

    automatically update method during trial initialization. New stimulus intensity can be further accessed by calling self.data['intensity'

--- core/BaseTrial.py
+++ core/BaseTrial.py
@@ -467,6 +467,12 @@ class BaseTrial(StateMachine):
             for label, value in self.triggers.iteritems():
                 self.triggers[label] = False
 
+            # Update experiment method
+            if hasattr(self.trial, 'method') and self.trial.method is not None and hasattr(self.trial.method, 'update'):
+                intensity = self.trial.method.update(int(self.trial.params['staircaseID']),
+                                                     int(self.trial.params['staircaseDir']))
+                self.data['intensity'] = intensity
+
             # Send START_TRIAL to devices
             for device in self.devices:
                 if hasattr(self.devices[device], 'start_trial'):
