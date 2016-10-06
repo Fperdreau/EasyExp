a87c53ebb30e60df0e4d4ef8ff53c6c972ef2ec6
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-10-06 17:58:06 +0200

    Fixed Devices container

--- core/Devices.py
+++ core/Devices.py
@@ -23,15 +23,13 @@ This file is for Devices class - dependencies container
 """
 
 from __future__ import print_function
-import sys
 import types
-
 import time
 
 from Config import ConfigFiles
 import logging
 
-from Core import Core
+from core.gui.gui_wrapper import GuiWrapper
 
 __status__ = "Development"
 
@@ -41,27 +39,107 @@ class Devices(object):
     Dependency container for devices
     """
 
-    def __init__(self, core=Core):
+    def __init__(self, exp_folder, base_name, cli=False):
         """
         Devices constructor
-        :param core: EasyExp.core instance
-        :type core: Core
+        :param exp_folder: path to experiment folder
+        :type exp_folder: str
+        :param base_name: data file base name
+            i.e "path/to/folder/subject_name"
+        :type base_name: str
         """
-        self.__core = core
-        self.devices = dict()
-        self.devices_file = ConfigFiles('{}/devices.json'.format(core.folders['expFolder']))
-        self.settingsFile = core.config
+        self.__devices_file = ConfigFiles('{}/devices.json'.format(exp_folder))
+        self.__base_name = base_name
         self.__logger = logging.getLogger('EasyExp')
+        self.__cli = cli
 
-    def init(self):
+        self.__devices = dict()
+        self.__ptw = None
+
+        # Get settings
+        self.__get_settings()
+
+    def init(self, ptw=None):
         """
         Instantiate and initialize devices
         """
-        self.devices_file.load()
-        for device_name, params in self.devices_file.data.iteritems():
-            self.add_device(device_name, **params)
+        # Load settings
+        self.__devices_file.load()
 
-    def add_device(self, device_name, **params):
+        # Set windows pointer if provided
+        if ptw is not None:
+            self.set_ptw(ptw)
+
+        # Add devices
+        for device_name, params in self.__devices_file.data['devices'].iteritems():
+            self.add(device_name, **params['options'])
+
+    def __get_settings(self):
+        """
+        Get devices settings
+        :return:
+        """
+        self.__devices_file.load()
+
+        if "settings" not in self.__devices_file.data:
+            self.__devices_file.data['settings'] = dict()
+
+        # Create/load settings for every device listed in devices.json
+        settings = dict()
+        for device in self.__devices_file.data['devices']:
+            if device not in self.__devices_file.data['settings']:
+                settings.update(self.get_field(device))
+            elif device in self.__devices_file.data['settings']:
+                settings.update({device: self.__devices_file.data['settings'][device]})
+
+        # Overwrite previous settings
+        self.__devices_file.data['settings'] = settings
+
+        # Allow user to change settings
+        data = {
+            'settings': self.__devices_file.data['settings']
+        }
+        data = self.display(data, cli=self.__cli)
+
+        # Update and save settings
+        self.__devices_file.data['settings'].update(data['settings'])
+        self.__devices_file.save()
+
+    def __getitem__(self, item):
+        """
+        Return instance of device
+        :param item:
+        :type item: str
+        :return:
+        """
+        if item.lower() in self.__devices:
+            return self.__devices[item.lower()]
+
+    def __iter__(self):
+        """
+        Return iterator
+        :return:
+        """
+        for item in self.__devices:
+            yield item
+
+    def iteritems(self):
+        """
+        Return iterator
+        :return:
+        """
+        for item, instance in self.__devices.iteritems():
+            yield item, instance
+
+    def set_ptw(self, ptw):
+        """
+        Set display pointer
+        :param ptw:
+        :return:
+        """
+        self.__ptw = ptw
+
+    def add(self, device_name, **params):
         """
         Add device to container
         :param device_name: name of device
@@ -69,42 +147,105 @@ class Devices(object):
         :param params: parameters passed to device's constructor
         :return:
         """
-        device = Devices.str_to_class(device_name)
-        if device_name not in self.devices and self.settingsFile.data['device'][device_name] is not False:
+        device = Devices.get_class(device_name)
+
+        if device_name.lower() not in self.__devices \
+                and self.__devices_file.data['settings'][device_name]['value'] is not "off":
+
+            if hasattr(device, "user_file"):
+                user_file = '{}_{}_{}.txt'.format(self.__base_name, device_name,
+                                                  time.strftime('%d-%m-%Y_%H%M%S'))
+                params.update({"user_file": user_file})
+
+            if hasattr(device, "dummy_mode"):
+                params.update({"dummy_mode": self.__devices_file.data['settings'][device_name]['value'] == 'dummy'})
+
+            if hasattr(device, "ptw"):
+                params.update({"ptw": self.__ptw})
+
+            # Instantiate device
             try:
-                if hasattr(device, "user_file"):
-                    user_file = '{}/{}_{}.txt'.format(self.__core.user.dftName, device_name,
-                                                      time.strftime('%d-%m-%Y_%H%M%S'))
-                    params.update({"user_file": user_file})
-
-                if hasattr(device, "ptw"):
-                    params.update({"ptw": self.__core.screen.ptw})
-
-                self.devices[device_name] = device(**params)
+                self.__devices[device_name.lower()] = device(**params)
+                self.__logger.info('Device "{}" successfully added.'.format(device_name.upper()))
             except AttributeError:
-                raise AttributeError('Could not instantiate {}'.format(device_name))
+                msg = AttributeError('Could not instantiate {}'.format(device_name))
+                self.__logger.critical(msg)
+                raise msg
 
-    def del_device(self, device_name):
+            # Start device if not self-started
+            if hasattr(self.__devices[device_name.lower()], 'run'):
+                try:
+                    self.__devices[device_name.lower()].run()
+                    self.__logger.info('Device "{}" successfully started.'.format(device_name.upper()))
+                except RuntimeError as e:
+                    msg = AttributeError('Could not start {}'.format(device_name))
+                    self.__logger.critical(msg)
+                    raise msg
+
+    def delete(self, device_name):
         """
         Removed device
         :param device_name: device label
         :type device_name: str
         :return:
         """
-        if device_name in self.devices:
-            del self.devices[device_name]
+        if device_name.lower() in self.__devices:
+            del self.__devices[device_name.lower()]
 
     @staticmethod
-    def str_to_class(field):
+    def get_class(method_name):
         """
         Instantiate class from class's name
-        :param field: class' name
-        :type field: str
+        :param method_name: lass' name
+        :type method_name: str
+        :return:
         """
+        # Import method implementation
+        method_path = "core.apparatus.{}.{}".format(method_name.lower(), method_name.lower())
         try:
-            identifier = getattr(sys.modules[__name__], field)
-        except AttributeError:
-            raise NameError("%s doesn't exist." % field)
-        if isinstance(identifier, (types.ClassType, types.TypeType)):
-            return identifier
-        raise TypeError("%s is not a class." % field)
+            mod = __import__(method_path, fromlist=method_name)
+            method = getattr(mod, method_name)
+            if isinstance(method, (types.ClassType, types.TypeType)):
+                return method
+            raise TypeError("%s is not a class." % method_name)
+        except ImportError as e:
+            msg = ImportError('[{}] Could not import "{}": {}'.format(__name__, method_name, e))
+            logging.getLogger('EasyExp').critical(msg)
+            raise msg
+
+    @staticmethod
+    def get_field(name):
+        """
+        Get field
+        :param name:
+        :type name: str
+        :rtype: dict
+        """
+        return {
+            name: {
+                "type": "select",
+                "options": [
+                    "on",
+                    "off",
+                    "dummy"
+                ],
+                "value": "off",
+                "label": name.capitalize()
+            },
+        }
+
+    @staticmethod
+    def display(data_to_load, cli=False):
+        """
+        Display settings and allow user to modify them
+        :param data_to_load: dictionary to display and modify
+        :type data_to_load: dict
+        :param cli: View
+        :type cli: bool
+        :rtype: dict
+        """
+        if cli is True:
+            info = GuiWrapper.factory(cli, 'nested', data_to_load, title="Devices Setup", mandatory=False)
+        else:
+            info = GuiWrapper.factory(cli, 'nested', data_to_load, title="Devices Setup")
+        return info.out
