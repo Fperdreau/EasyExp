3e6b7423c479dfa58d781576ce105621b0a1fa05
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-11-06 19:38:40 +0100

    removed staircase

--- tests/testTrial.py
+++ tests/testTrial.py
@@ -9,7 +9,7 @@ import sys
 import numpy as np
 
 # Environmental settings
-root_folder = dirname(dirname(abspath('__dir__')))
+root_folder = dirname((abspath('__dir__')))
 sys.path.append("{}/libs".format(root_folder))
 from core.Core import Core
 from core.Trial import Trial
@@ -20,85 +20,26 @@ def main():
     """
     Main function: call experiment's routines
     """
-    options = {
-        'stimRange': (-2.8, 2.8),                    # Boundaries of stimulus range
-        'Pfunction': 'cGauss',                  # Underlying Psychometric function
-        'nTrials': 50,                          # Number of trials per staircase
-        'threshold': np.linspace(-2.8, 2.8, 20),                      # Threshold estimate
-        'thresholdPrior': ('uniform', None),    # Prior on threshold
-        'slope':  np.logspace(-3.0, 3.0, 20),                          # Slope estimate
-        'slopePrior': ('uniform', None),        # Prior on slope
-        'guessRate': np.linspace(0.40, 0.60, 20),                      # Guess-rate estimate
-        'guessPrior': ('uniform', None),        # Prior on Guess-rate
-        'lapseRate': np.linspace(0.0, 0.05, 20),                      # Lapse-rate estimate
-        'lapsePrior': ('uniform', None),        # Prior on lapse-rate
-        'marginalize': True,                    #
-        'nbStairs': 1,                          # Number of staircase per condition
-        "warm_up": 4,                           # Number of warm-up trials per staircase (only extremes intensity)
-        "response_field": "response",           # Name of response field in data file
-        "intensity_field": "intensity"          # Name of intensity field in data file
-    }
 
-    print(root_folder)
     # New experiment
     Exp = Core()
 
     Exp.init(root_folder, custom=False)
 
     # Instantiate Trial and experiment
-    trial = Trial(design=Exp.design, settings=Exp.config, userfile=Exp.user.datafilename,
-                  pause_interval=int(Exp.config['pauseInt']))
+    trial = Trial(design=Exp.design, settings=Exp.config.settings, userfile=Exp.user.datafilename,
+                  pause_interval=int(Exp.config.settings['setup']['pauseInt']))
 
-    # staircase = StairCaseASA(settings_file=Exp.files['conditions'], data_file=Exp.user.datafilename)
-    staircase = PsiMarginal(settings_file=Exp.files['conditions'], data_file=Exp.user.datafilename, options=options)
-    stairs = {}
     while trial.status is not False:
         trial.setup()
 
-        if trial.status is not False:
-            intensity = staircase.update(int(trial.params['staircaseID']), int(trial.params['staircaseDir']))
-            print('intensity: {}'.format(intensity))
-            if trial.params['staircaseID'] not in stairs:
-                stairs.update({trial.params['staircaseID']: {'true': np.random.uniform(-2.0, 2.0)}})
+        if trial.status is True:
 
-            mu = stairs[trial.params['staircaseID']]['true']
-            sd = 0.1  # std
-            internal_rep = intensity + sd * np.random.normal()
-            resp_curr = (abs(internal_rep) > abs(mu))
-            print('ID: {} mu: {} internal: {} response: {} noise: {}'.format(trial.params['staircaseID'], mu,
-                  internal_rep, resp_curr, np.random.normal()*sd))
-            valid = np.random.uniform(0.0, 1.0) < 0.8
+            valid = np.random.uniform(0.0, 1.0) < 0.95
             trial.stop(valid)
-            trial.write_data({'intensity': intensity, 'correct': resp_curr})
+            trial.write_data({'intensity': 0, 'correct': 'left'})
 
     data = trial.load_data()
-
-    res = {}
-    for trial in data:
-        if trial['Replay'] == 'False':
-            if trial['staircaseID'] not in res:
-                res[trial['staircaseID']] = []
-
-            res[trial['staircaseID']].append(float(trial['intensity']))
-
-    # Plot staircase results
-    import matplotlib.pyplot as plt
-    trues = []
-    estimates = []
-    for stair, intensities in res.iteritems():
-        stairs[stair]['estimate'] = intensities[len(intensities)-1]
-        trues.append(stairs[stair]['true'])
-        estimates.append(stairs[stair]['estimate'])
-        plt.plot(intensities)
-    plt.show()
-
-    plt.scatter(trues, estimates)
-    plt.xlabel('True mean')
-    plt.ylabel('Estimates')
-    plt.show()
-
-    from pprint import pprint
-    pprint(stairs)
 
     # Check that all the conditions have been played
     levels = Exp.design.allconditions['timing']
