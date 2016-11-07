38408e0b755527fae1928eceef3b6b5803e79e2a
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-11-07 11:43:24 +0100

    Added Feedback for continuous measurements

--- core/events/feedback.py
+++ core/events/feedback.py
@@ -19,8 +19,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import numpy as np
 
-__version__ = '1.0.0'
+_version_ = "1.1.0"
 
 
 class FeedBack(object):
@@ -52,8 +53,9 @@ class FeedBack(object):
         :return: True if is time to update. False otherwise
         """
         self._count += 1
-        print('Feedback count: {}'.format(self._count))
         do_update = self._count >= self._frequency
+        logging.getLogger('EasyExp').info('Feedback: {}({})'.format(do_update, self._count))
+
         if do_update:
             self._count = 0
         return do_update
@@ -64,7 +66,10 @@ class FeedBack(object):
         Compute progression since last feedback
         :return:
         """
-        self._progression = ((self._score - self._prev_score) / self._prev_score) * 100.0
+        if self._prev_score != 0.0:
+            self._progression = ((self._score - self._prev_score) / self._prev_score) * 100.0
+        else:
+            self._progression = 0.0
         return self._progression
 
     @property
@@ -88,8 +93,8 @@ class FeedBack(object):
 
         if len(data) > 0 and played > 0:
             correct_trials = [data[ii] for ii in range(len(data)) if data[ii][self._field] == 'True']
-            n_correct = len(correct_trials)
-            score = (n_correct / played) * 100.0
+            n_correct = float(len(correct_trials))
+            score = (n_correct / float(played)) * 100.0
         else:
             score = 0.0
 
@@ -97,4 +102,89 @@ class FeedBack(object):
         self._score = score
 
     def __str__(self):
-        return 'Score: {0: 1.1f}% | Progression: {1:1.1f}%'.format(self._score, self._progression)
+        return 'Score: {0: 1.1f}% | Progression: {1:1.1f}%'.format(self._score, self.progression)
+
+
+class FeedBackContinuous(FeedBack):
+    """
+    Extension of Feedback class to support continuous measurements
+    """
+
+    def update(self, data, played):
+        """
+        Get feedback message indicating current score and progression since last feedback
+        :param data: data array
+        :type data: array-like (trial x property)
+        :param played: number of played trials
+        :type played: int
+        :return void
+        """
+
+        if len(data) > 0 and played > 0:
+            correct_trials = [float(data[ii][self._field]) for ii in range(len(data))]
+            score = np.mean(correct_trials)
+        else:
+            score = 0.0
+
+        self._prev_score = self._score
+        self._score = score
+
+    def __str__(self):
+        return 'Score: {0: 1.1f} | Progression: {1:1.1f}%'.format(self._score, self.progression)
+
+
+if __name__ == '__main__':
+
+    data = []
+    fb = FeedBack(5)
+
+    for t in range(50):
+        if fb.time_to_update:
+            fb.update(data, t)
+            print(fb)
+
+        if np.random.random() >= 0.5:
+            correct = 'True'
+        else:
+            correct = 'False'
+        data.append({'correct': correct})
+
+    all_data = [{'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '17',
+      'response': '96.591607869660436', 'landmark_distance': '100'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '-1', 'TrialID': '42',
+      'response': '-108.35165428884542', 'landmark_distance': '-1'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '57',
+      'response': '253.57736661891465', 'landmark_distance': '100'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '-1',
+      'TrialID': '226', 'response': '-44.954378875942616', 'landmark_distance': '50'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '291',
+      'response': '123.23584900561099', 'landmark_distance': '-1'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '380',
+      'response': '77.278286187005889', 'landmark_distance': '200'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '-1', 'TrialID': '163',
+      'response': '23.545132241295534', 'landmark_distance': '0'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '-1',
+      'TrialID': '242', 'response': '-56.710505329889614', 'landmark_distance': '-1'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '-1', 'TrialID': '221',
+      'response': '127.06889654653492', 'landmark_distance': '-1'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '100',
+      'response': '74.534348734929978', 'landmark_distance': '200'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '295',
+      'response': '25.993236024661659', 'landmark_distance': '50'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '134',
+      'response': '-37.124957569462985', 'landmark_distance': '0'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '275',
+      'response': '12.401668129059871', 'landmark_distance': '50'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '-1',
+      'TrialID': '144', 'response': '87.055586652654227', 'landmark_distance': '0'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'left', 'Replay': 'False', 'landmark_side': '-1', 'TrialID': '205',
+      'response': '-107.02315708640468', 'landmark_distance': '50'},
+     {'landmark_status': '1', 'mvt': 'True', 'side': 'right', 'Replay': 'False', 'landmark_side': '1', 'TrialID': '200',
+      'response': '66.433153579148382', 'landmark_distance': '200'}]
+
+    fb = FeedBackContinuous(5, field='response')
+    for t in range(len(all_data)):
+        if fb.time_to_update:
+            data = [all_data[ii] for ii in range(0, t)]
+            fb.update(data, t)
+            print(fb)
